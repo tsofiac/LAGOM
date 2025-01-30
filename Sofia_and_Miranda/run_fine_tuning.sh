6329a0951fee2b238d5367c55a7f78b4395fee0d
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-01-30 15:02:19 +0000

    inference_score file created. Pretrained model used

--- Sofia_and_Miranda/run_fine_tuning.sh
+++ Sofia_and_Miranda/run_fine_tuning.sh
@@ -2,9 +2,9 @@
 ./Sofia_and_Miranda/fine_tune.sh \
     Sofia_and_Miranda/fine_tune_bart.yaml \
     Sofia_and_Miranda/testdata.csv \
-    null \
+    Sofia_and_Miranda/pretrained_model/step=1000000_mod_no_deepspeed.ckpt \
     64 \
-    tests/chemformer/data/simple_vocab.json \
+    Sofia_and_Miranda/bart_vocab_downstream.json \
     Sofia_and_Miranda/results/training/ \
     Sofia_and_Miranda/results/training/config.yaml
 
