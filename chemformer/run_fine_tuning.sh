6096df757703c7cfdc1454d6ac414d8cca4c2443
kbvs588_azu <sofia.larsson3@astrazeneca.com>
2025-02-05 15:12:22 +0000

    standard smiles added

--- chemformer/run_fine_tuning.sh
+++ chemformer/run_fine_tuning.sh
@@ -1,12 +1,12 @@
 # sbatch fine_tune.sh \
-./Sofia_and_Miranda/fine_tune.sh \
-    Sofia_and_Miranda/fine_tune_bart.yaml \
-    Sofia_and_Miranda/unique_parents_metxbiodb_finetuning.csv \
+.chemformer/fine_tune.sh \
+    chemformer/fine_tune_bart.yaml \
+    dataset/dummy_data/unique_parents_metxbiodb_finetuning.csv \
     step=1000000_mod_no_deepspeed.ckpt \
     64 \
-    Sofia_and_Miranda/bart_vocab_downstream.json \
-    Sofia_and_Miranda/results/training/ \
-    Sofia_and_Miranda/results/training/config.yaml
+    chemformer/bart_vocab_downstream.json \
+    results/training \
+    results/training/config.yaml
 
 # sbatch fine_tune.sh \
 #     fine_tune_bart.yaml \
---
+++ preprocessing/standardize_smiles.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Loading the Python packages
+
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem.SaltRemover import SaltRemover
+from chembl_structure_pipeline.standardizer import standardize_mol
+
+# Defining functions
+
+def standardize_molecule(smiles, isomericSmiles=True):
+    
+    '''
+    Standardization of SMILES by standardiser;
+    '''
+    
+    try:
+        rm = SaltRemover()
+        mol = Chem.MolFromSmiles(smiles)
+        mol = rm.StripMol(mol, dontRemoveEverything=True)
+        if len(Chem.MolToSmiles(mol).split('.')) > 1:
+            salt = Chem.MolToSmiles(mol)
+            frag_dict = {len(k): k for k in salt.split('.')}
+            max_frag = frag_dict[max(list(frag_dict.keys()))]
+            mol = Chem.MolFromSmiles(max_frag)
+        mol = standardize_mol(mol)
+        clean_smiles = Chem.MolToSmiles(mol, isomericSmiles=isomericSmiles) #removing (False) or keeping (True) stereochemistry
+        if Chem.MolFromSmiles(clean_smiles) is None:
+            clean_smiles = None  
+    except:
+        clean_smiles = np.nan
+    return clean_smiles
+
+def standardize_smiles_collection(smiles_list, isomericSmiles=True):
+    
+    '''
+    Standardization of SMILES collection by standardiser;
+    '''
+    
+    lookup = {}
+    return_smiles = []
+    for smiles in smiles_list:
+        if smiles in lookup:
+            standardized_smiles = lookup[smiles]
+        else:
+            standardized_smiles = standardize_molecule(smiles, isomericSmiles=isomericSmiles)
+            lookup[smiles] = standardized_smiles
+        return_smiles.append(standardized_smiles)
+        
+    return return_smiles
