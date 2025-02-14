c06e236b3893a5f426d932315f4c476f1eede8c2
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-02-14 10:38:30 +0000

    filtering on fingerprints

--- preprocessing/preprocessing_filter.py
+++ preprocessing/preprocessing_filter.py
@@ -68,15 +68,67 @@ def atoms_allowed_in_molecules(molecule):
     atoms = [atom.GetSymbol() for atom in mol.GetAtoms()]
     return set(atoms).issubset(set(atoms_to_include)) #Returns true if all atoms in the molecule are included in the specified set
 
+def define_finger_print_similarity(dataset):
+    parent_smiles = dataset['parent_smiles'].tolist()
+    child_smiles = dataset['child_smiles'].tolist()
 
+    parent_mol = [Chem.MolFromSmiles(x) for x in parent_smiles]
+    child_mol = [Chem.MolFromSmiles(x) for x in child_smiles]
+
+    parent_fps = [AllChem.GetMorganFingerprintAsBitVect(x, radius=2, nBits=1024) for x in parent_mol]
+    child_fps = [AllChem.GetMorganFingerprintAsBitVect(x, radius=2, nBits=1024) for x in child_mol]
+
+    fingerprint_similarities = []
+    for i in range(len(parent_smiles)):
+        
+        s = DataStructs.TanimotoSimilarity(parent_fps[i], child_fps[i]) 
+        fingerprint_similarities.append(s)
+
+    dataset['tanimoto'] = fingerprint_similarities
+
+    return dataset
+
+def fingerprints_allowed(similarity, min_similarity):
+    if similarity < min_similarity or similarity == 1:
+        return False
+    return True
+
+def finger_print_similarity_filter(data, min_similarity = 0.15):
+    data = define_finger_print_similarity(data)
+
+    total_removed = 0
+
+    allowed_molecules = [fingerprints_allowed(value, min_similarity) for value in data["tanimoto"]]
+    filtered_data = data[allowed_molecules]
+    removed_data = data[[not allowed for allowed in allowed_molecules]]
+    total_removed += allowed_molecules.count(False)
+
+    return filtered_data, removed_data, total_removed
+
+# ---------------------------------
+
+name = 'metxbiodb'
+df = pd.read_csv(f'dataset/curated_data/{name}_clean_unique_parents.csv')
+
+filtered_data, removed_data, total_removed = finger_print_similarity_filter(df)
+filtered_data.to_csv(f'dataset/curated_data/filtered/filtered_fingerprint_{name}_clean_unique_parents.csv', index=False)
+
+print("Total data points removed: ", total_removed)
+print(removed_data)
+
+
+# ---------------------------------
+
+'''
 name = 'metxbiodb' # [ 'drugbank' 'metxbiodb' ]'
-filter_method = atoms_allowed_in_molecules #remember to change file name
+filter_method = valid_smiles #remember to change file name
 
 df = pd.read_csv(f'dataset/curated_data/{name}_clean_unique_parents.csv')
 
 filtered_data, removed_data, total_removed=filter_out_data_on_parent_and_child(df, filter_method)
 
-filtered_data.to_csv(f'dataset/curated_data/filtered/filtered_atoms_{name}_clean_unique_parents.csv', index=False)
+filtered_data.to_csv(f'dataset/curated_data/filtered/filtered_valid_smiles_{name}_clean_unique_parents.csv', index=False)
 
 print("Total data points removed: ", total_removed)
 print(removed_data)
+'''
