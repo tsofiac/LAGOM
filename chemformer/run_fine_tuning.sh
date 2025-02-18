b1a8037f8a16252362c2e5795d57ced9b458f620
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-02-18 15:53:02 +0000

    new files

--- chemformer/run_fine_tuning.sh
+++ chemformer/run_fine_tuning.sh
@@ -1,7 +1,7 @@
 # sbatch fine_tune.sh \
 ./chemformer/fine_tune.sh \
     chemformer/fine_tune_bart.yaml \
-    dataset/curated_data/filtered/filtered_fingerprint_drugbank_clean_unique_parents.csv \
+    dataset/finetune/combined_unique_parents_finetune.csv \
     step=1000000_mod_no_deepspeed.ckpt \
     64 \
     chemformer/bart_vocab_downstream.json \
