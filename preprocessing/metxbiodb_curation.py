1653b1f8fe5f4da138b3052dbac7b80fa3e80359
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-02-21 16:50:37 +0000

    counting in metxbio curation

--- preprocessing/metxbiodb_curation.py
+++ preprocessing/metxbiodb_curation.py
@@ -79,6 +79,8 @@ def check_origin_vs_parent(data_file): #just for checking
     # Read the DataFrame
     df = pd.read_csv(data_file)
 
+    counter = 0
+
     # Check for discrepancies where 'origin' is not equal to 'parent_name'
     discrepancy_exists = False
     for index, row in df.iterrows():
@@ -86,7 +88,9 @@ def check_origin_vs_parent(data_file): #just for checking
             discrepancy_exists = True
             print(f"Discrepancy found at index {index}:")
             print(f"\tParent Name: {row['parent_name']}, Origin: {row['origin']}")
+            counter += 1
 
+    print("Nr of times where the parent and origin are not the same: ", counter)
     return discrepancy_exists
 
 if __name__ == "__main__":
