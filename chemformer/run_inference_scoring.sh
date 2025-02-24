d808eb15107920ef0a427519bbf4f877bbfb778b
kbvs588_azu <sofia.larsson3@astrazeneca.com>
2025-02-24 15:37:54 +0000

    commit

--- chemformer/run_inference_scoring.sh
+++ chemformer/run_inference_scoring.sh
@@ -1,11 +1,11 @@
 # sbatch inference_score.sh \
 ./chemformer/inference_score.sh \
-    inference_score.yaml \
-    dataset/curated_data/gloryx_smiles_clean.csv \
-    results/training/forward_prediction/version_0/checkpoints/epoch=10-step=88.ckpt \
+    chemformer/inference_score.yaml \
+    dataset/finetune/gloryx_finetune.csv \
+    results/training/forward_prediction/version_2/checkpoints/epoch=10-step=88.ckpt \
     chemformer/bart_vocab_downstream.json \
     64 \
-    1 \ 
+    1 \
     forward_prediction \
     results/evaluation/metrics_scores \
     results/evaluation/predictions \
