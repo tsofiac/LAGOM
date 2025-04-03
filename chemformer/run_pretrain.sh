d6e3fd0aade7a2f66415ab8f63ec2301ba084459
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-04-03 11:37:40 +0000

    pretrain files

--- chemformer/run_pretrain.sh
+++ chemformer/run_pretrain.sh
@@ -1,11 +1,13 @@
-    sbatch chemformer/fine_tune.sh \
+    sbatch chemformer/pretrain.sh \
     chemformer/pretrain_bart.yaml \
-    dataset/finetune/mmp_all_finetune.csv\
-    null \
-    128 \
+    dataset/finetune/mmp_100000rows_finetune.csv \
+    step=1000000_mod_no_deepspeed.ckpt \
+    500 \
+    0 \
+    0 \
     chemformer/bart_vocab_downstream.json \
-    results/pretraining \
-    results/pretraining/config.yaml
+    results/pretraining/base \
+    results/pretraining/base/config.yaml
 
 # sbatch fine_tune.sh \
 #     fine_tune_bart.yaml \
