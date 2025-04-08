243738e716161b3e83f1f9baa63496c1ce0d5128
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-04-08 15:10:28 +0000

    mmp sh

--- preprocessing/submit_preprocess.sh
+++ preprocessing/submit_preprocess.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=12G
+#SBATCH --mem=50G
 #SBATCH -p long,vlong
 #SBATCH --output=slurm-output-%j.log
 #SBATCH --error=slurm-error-%j.log
@@ -16,6 +16,6 @@ conda activate aizynthmodels
 export OMP_NUM_THREADS=12
  
 # start the calculation
-python preprocessing/load_mmp_parts.py  # <-- här skriver ni bara namnet på ert script efter "python". NOTERA: detta script måste ligga i samma directory som python scriptet!!
+python preprocessing/NEW_preprocessing_all.py  # <-- här skriver ni bara namnet på ert script efter "python". NOTERA: detta script måste ligga i samma directory som python scriptet!!
  
 # end of script
