703f275ce29aa41ea32372b5586c2b0cd13485e3
kbvs588_azu <sofia.larsson3@astrazeneca.com>
2025-04-28 11:19:24 +0000

    updated final csv

---
+++ preprocessing/generate_non-canonical.py
@@ -0,0 +1,24 @@
+from rdkit import Chem
+import numpy as np
+from random import shuffle
+from chembl_structure_pipeline.standardizer import standardize_mol
+
+# Canonicalise
+smiles = 'CC(=O)OC1=CC=CC=C1C(O)=O'
+mol = Chem.MolFromSmiles(smiles)
+mol = standardize_mol(mol)
+smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=False)
+print(smiles)
+
+# Randomise to non-canonicalise
+mol = Chem.MolFromSmiles(smiles)
+atom_order = list(range(mol.GetNumAtoms()))
+np.random.shuffle(atom_order)
+mol_rand = Chem.RenumberAtoms(mol, atom_order)
+new_smiles = Chem.MolToSmiles(mol_rand, canonical=False)
+print(new_smiles)
+
+# Verify same smiles
+new_mol = Chem.MolFromSmiles(new_smiles)
+new_smiles = Chem.MolToSmiles(new_mol, canonical=True)
+print(new_smiles)
