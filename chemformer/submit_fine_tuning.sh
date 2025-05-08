72f955e590eba20cf65b860c76f40c109cb075df
kbvs588_azu <sofia.larsson3@astrazeneca.com>
2025-05-08 06:33:19 +0000

    submit fine tune

--- chemformer/submit_fine_tuning.sh
+++ chemformer/submit_fine_tuning.sh
@@ -1,11 +1,11 @@
 sbatch chemformer/fine_tune_submit.sh \
     chemformer/fine_tune_bart_submit.yaml \
-    dataset/finetune/metatrans_finetune.csv \
-    results/pretraining/ChemVA/forward_prediction/version_2/checkpoints/last.ckpt \ #CHECKA
+    dataset/alohomora/PG_PP_finetune.csv \
+    pre-trained_models/ChemVA_comb \
     64 \
     chemformer/bart_vocab_downstream.json \
-    results/training/meta/ \
-    results/training/meta/config.yaml
+    results/training/alohomora/ \
+    results/training/alohomora/config.yaml
 
 # sbatch fine_tune.sh \
 #     fine_tune_bart.yaml \
