21e7cbaafe604e4b366d01525530943f84ca8af6
ksmq649_azu <miranda.carlsson@astrazeneca.com>
2025-05-28 14:55:07 +0000

    updated combine_mmp

--- preprocessing/combine_mmp.py
+++ preprocessing/combine_mmp.py
@@ -1,40 +1,5 @@
 import pandas as pd
 
-    # rows for mmp
-    # start_row = 0
-    # end_row = 1101304
-
-    # start_row = 1101304
-    # end_row = 2202607
-
-    # start_row = 2202607
-    # end_row = 3303912
-
-    # start_row = 3303912
-    # end_row = 4405216 
-
-    # start_row = 4405216
-    # end_row = 5506519
-
-    # start_row = 5506519
-    # end_row = 6607824
-
-    # start_row = 6607824
-    # end_row = 7709127
-
-    # start_row = 7709127
-    # end_row = 8810431
-
-    # start_row = 8810431
-    # end_row = 9911735
-
-    # start_row = 9911735
-    # end_row = None #11013037
-
-
-
-import pandas as pd
-
 def combine_filtered_mmp_files(combined_mmp_file):
     # Base directory for storing or referencing the CSV files
     base_dir = '/projects/cc/se_users/carlsson_ksmq649/MasterThesis/dataset/mmp'
@@ -75,5 +40,5 @@ def combine_filtered_mmp_files(combined_mmp_file):
     # Save the combined DataFrame to a new CSV file
     df_combined.to_csv(combined_mmp_file, index=False)
 
-combined_mmp_file = 'dataset/mmp/mmp_all_smiles_clean_test.csv' 
+combined_mmp_file = 'dataset/mmp/mmp_all_smiles_clean.csv' 
 combine_filtered_mmp_files(combined_mmp_file)
